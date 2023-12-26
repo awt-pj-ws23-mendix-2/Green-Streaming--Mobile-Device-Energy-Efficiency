db61fcbcbfa3fe81f07d5a5c904072d3ad512e8b
krutarth4 <krutarthparwal@gmail.com>
2023-12-26 16:59:25 +0100

    parsing and relocation

--- src/parser.py
+++ src/parser.py
@@ -1,14 +1,22 @@
 import configparser
-
+import os
 
 class Parser:
 
     def __init__(self):
         self.config = configparser.ConfigParser()
-        self.config.read("./src/configuration.config")
+        config_path = os.path.join(os.getcwd(), "configuration.config")
+        self.config.read(config_path)
 
     def get_output_path(self):
-        return self.config.get("Path", 'output_path')
+        path= self.config.get("Path", 'output_path')
+        return path
 
     def get_credentials(self):
         return self.config.get("Password", 'sudo_password')
+
+# if __name__ == "__main__":
+#     print("starting")
+#     pd = Parser()
+#     # print(pd.get_credentials())
+#     print(pd.get_output_path())
--- src/power_metrics.py
+++ src/power_metrics.py
@@ -1,25 +1,30 @@
 import subprocess
-import configparser
+import parser
+
+
 class powermetrics:
- # TODO: filter the remainig form for energy impact
- # TODO: save it in a file
- # TODO: Execute multi processing or async calls to the data
+    # TODO: filter the remainig form for energy impact
+    # TODO: save it in a file
+    # TODO: Execute multi processing or async calls to the data
 
     @staticmethod
     def gather_data(number_of_samples, interval):
-        sudo_command=f"sudo -S powermetrics -i {number_of_samples} -n {interval} --show-process-energy | grep -iE 'ffplay|ALL_TASKS|NAME'"
+        config_data = parser.Parser()
+        sudo_command = f"sudo -S powermetrics -i {number_of_samples} -n {interval} --show-process-energy | grep -iE 'ffplay|ALL_TASKS|NAME'"
         print(sudo_command)
         # output = subprocess.check_output(sudo_command, shell=True, universal_newlines=True ,input='2252')
-        output = subprocess.run(sudo_command, shell=True, universal_newlines=True ,input='2252',stdout=subprocess.PIPE, text=True)
+        output = subprocess.run(sudo_command, shell=True, universal_newlines=True, input=config_data.get_credentials(),
+                                stdout=subprocess.PIPE, text=True)
         output = output.stdout
         print(output)
-        with open('./src/temp_data/output.txt', 'w') as file:
+        with open(config_data.get_output_path(), 'w') as file:
             file.write(output)
         # filter='awk {printf "%s ", $NF}'
         # output = subprocess.check_output(filter, shell=True, universal_newlines=True)
         # print("new output")
         # print(output)
 
+
 if __name__ == "__main__":
     pd = powermetrics()
-    pd.gather_data(100,20)
+    pd.gather_data(100, 20)
