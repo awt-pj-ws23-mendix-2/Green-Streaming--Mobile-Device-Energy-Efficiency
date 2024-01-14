b85c29092c75f466a5c3fd6a838feb272346b0ec
krutarth4 <krutarthparwal@gmail.com>
2024-01-14 07:58:18 +0100

    file manager test added

--- src/file_management.py
+++ src/file_management.py
@@ -1,24 +1,26 @@
 import re
+import os
+
 
 class FileManagement:
     def __init__(self):
         pass
 
-    def get_filename(self,path):
+    def get_filename(self, path):
         match = re.search(r'([^/\\]+)\.mp4$', path)
         return match.group(1)
 
+    def write_file(self, output, file_path):
+        if os.path.exists(file_path):
+            with open(file_path, 'a') as file:
+                file.write(output)
+            print(f"Data successfully written to {file_path}")
+        else:
+            print(f"Error: File '{file_path}' does not exist.")
 
-if __name__=="__main__":
-    video_path = "./testfolder/output.mp4"
 
-    # Use regular expression to extract the word before ".mp4"
-    match = re.search(r'([^/\\]+)\.mp4$', video_path)
-
-    if match:
-        word_before_mp4 = match.group(1)
-        print("Word before .mp4:", word_before_mp4)
-    else:
-        print(".mp4 extension not found in the given path.")
-
-    print(word_before_mp4+".txt")
+if __name__ == "__main__":
+    output = "File management works "
+    file_manager = FileManagement()
+    path = "./temp_data/file_manager.txt"
+    file_manager.write_file(output, path)
